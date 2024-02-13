0cadce46b86f199e46479b4b9c16a13aef913312
sapatevaibhav <sapatevaibhav@duck.com>
2024-02-13 16:30:23 +0530

    some minors

--- lib/pages/animals.dart
+++ lib/pages/animals.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api, deprecated_member_use, prefer_const_literals_to_create_immutables
+// ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api, deprecated_member_use, must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
