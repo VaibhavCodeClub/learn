bf3f67b742a4a78dcb586510a4f54dbc24a5ae4c
Ayan <imluckyayan@gmail.com>
2024-05-16 02:10:50 +0530

    defined model Module

---
+++ lib/model/module.dart
@@ -0,0 +1,13 @@
+abstract class Module {
+  final String name;
+  final String description;
+  final String thumbnailPath;
+  final String route;
+
+  Module({
+    required this.name,
+    required this.description,
+    required this.thumbnailPath,
+    required this.route, 
+  });
+}
