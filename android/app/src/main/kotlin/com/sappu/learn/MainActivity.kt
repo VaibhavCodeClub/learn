a78c08316cdedf0f34ddbca1462de0f042b321f1
Gaurav-Kushwaha-1225 <gaurav.family04@gmail.com>
2024-06-05 23:55:32 +0530

    Issue resolved but app icon and android folder has to be modified

--- android/app/src/main/kotlin/com/sappu/learn/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sappu.learn
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/vdrs/sappu/lafk/learn/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package vdrs.sappu.lafk.learn
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
