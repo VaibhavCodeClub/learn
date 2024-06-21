13c976641d11f700ea58c83a934bfb30ba532198
ppkekw <accplaceholder311@gmail.com>
2024-06-21 15:03:56 +0530

    removed unused imports and fixed WidgetSateProperty errors

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-import 'package:learn/utils/assets_path.dart';
 import 'package:learn/utils/route/route_constant.dart';
 
 class MyDrawer extends StatelessWidget {
